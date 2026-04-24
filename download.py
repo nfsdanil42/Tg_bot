400e7e5d54318da3f08a7fc7026e3c6e613771f8
nfsdanil42 <nfsdanil42@gmail.com>
2026-04-24 22:43:05 +0300

    Add files via upload

---
+++ download.py
@@ -0,0 +1,12 @@
+import os
+import json
+import requests
+import time
+os.environ["DISPLAY"]=":90"
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
